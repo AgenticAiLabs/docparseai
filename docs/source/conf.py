5f4534e71707fe452f08d96483d36e124618813e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-06-09 11:15:12 +0100

    added the source files, updated pyproject.toml file

---
+++ docs/source/conf.py
@@ -0,0 +1,14 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'docparseai'
+copyright = '2025, Paul Onyekwelu'
+author = 'Paul Onyekwelu'
+release = '0.1.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
